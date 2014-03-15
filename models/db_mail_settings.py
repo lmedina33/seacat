659e644b50fbba8ce4cbc80571d055b32d41ade0
myakimavicius@gmail.com <myakimavicius@gmail.com>
2014-03-15 20:14:30 -0300

    -Actualizaciòn controladores para acceso remoto a servidor Pio. -Intento de arreglo problema al finalizar Survey (sin probar aùn)

--- models/db_mail_settings.py
+++ models/db_mail_settings.py
@@ -1,5 +1,9 @@
 # coding: utf8
 ## configure email
+MAIL_USER="soportetecnico"
+MAIL_DOMAIN="pioix.edu.ar"
+MAIL_PASSWORD="deagostini"
+## configure email
 mail = auth.settings.mailer
 mail.settings.server = 'logging' or 'smtp.gmail.com:587'
 mail.settings.server = 'smtp.gmail.com:587'
